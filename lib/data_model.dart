6cc1f9661a727fb75eca775c007ecfdaeae095d1
osamasandhu <osamasandhu3636@gmail.com>
2022-07-20 15:06:03 +0500

    data model

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -1,5 +1,3 @@
-import 'package:custom_data_table/data_table_page.dart';
-
 class DataTableModel {
   String name;
   String age;
@@ -15,7 +13,7 @@ class DataTableModel {
       required this.age});
 }
 
-List<DataTableModel> dummyTableData = [
+List<DataTableModel> tableData = [
   DataTableModel(
     name: 'Osama Sandhu',
     age: '22',
@@ -23,5 +21,90 @@ List<DataTableModel> dummyTableData = [
     address:
         '86/A near masjid al haq  Garden city stadium chowk vehari road Multan',
     email: 'osamasandhu3636@gmail.com',
-  )
+  ),
+  DataTableModel(
+    name: 'Haroon Ashraf Away',
+    age: '28',
+    phone: '03012144088',
+    address: 'Farrukh Town Khanewal Road Multan ',
+    email: 'haroonawan000@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Faisal Amer',
+    age: '16',
+    phone: '03187986256',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Karam ',
+    age: '33',
+    phone: '03112346498',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Haroon Ashraf Away',
+    age: '28',
+    phone: '03012144088',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Faisal Amer',
+    age: '16',
+    phone: '03187986256',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),DataTableModel(
+    name: 'Osama Sandhu',
+    age: '22',
+    phone: '03182220646',
+    address:
+        '86/A near masjid al haq  Garden city stadium chowk vehari road Multan',
+    email: 'osamasandhu3636@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Haroon Ashraf Away',
+    age: '28',
+    phone: '03012144088',
+    address: 'Farrukh Town Khanewal Road Multan ',
+    email: 'haroonawan000@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Faisal Amer',
+    age: '16',
+    phone: '03187986256',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Karam ',
+    age: '33',
+    phone: '03112346498',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Haroon Ashraf Away',
+    age: '28',
+    phone: '03012144088',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
+  DataTableModel(
+    name: 'Faisal Amer',
+    age: '16',
+    phone: '03187986256',
+    address:
+        'aksj cjksa cj sakj saj jas kjsajksakjkjaaskjsakjjkskjsajkkjsakjsak sksakjn',
+    email: 'ckssaklklsalk@gmail.com',
+  ),
 ];
